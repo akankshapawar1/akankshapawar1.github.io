16da241530ac10e6ab658e30c3851148ec1a8eba
Akanksha <42142815+akankshapawar1@users.noreply.github.com>
2024-09-06 13:16:03 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,6 @@ import ReactGA from "react-ga4";
 
 function App() {
   useEffect(() => {
-
-    console.log(
-      "%cIf you're interested in the code for this site, you can find it here:",
-      "font-size: 1.1rem; color: #FFF;"
-    );
-
-    console.log(
-      "%chttps://github.com/arhammusheer/croissant.one",
-      "font-size: 1.1rem; color: #4299E1;"
-    );
-
     ReactGA.initialize(ANALYTICS_MEASUREMENT_ID);
     ReactGA.send("pageview");
   }, []);
